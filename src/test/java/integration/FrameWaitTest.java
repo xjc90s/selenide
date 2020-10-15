8cfbe96802b4874f2f00595b5fb75f0d910e3914
Andrei Solntsev <andrei.solntsev@gmail.com>
2020-10-15 18:19:16 +0300

    Reproduce HtmlUnit issue #68

--- src/test/java/integration/FrameWaitTest.java
+++ src/test/java/integration/FrameWaitTest.java
@@ -1,12 +1,10 @@
 package integration;
 
-import com.automation.remarks.video.annotations.Video;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 
 import static com.codeborne.selenide.Condition.name;
 import static com.codeborne.selenide.Condition.text;
-import static org.junit.jupiter.api.Assumptions.assumeFalse;
 
 class FrameWaitTest extends ITest {
   @BeforeEach
@@ -14,28 +12,15 @@ class FrameWaitTest extends ITest {
     openFile("page_with_frames_with_delays.html");
   }
 
-  @Test
-  @Video
+  @RepeatedTest(100)
   void waitsUntilFrameAppears_inner() {
     switchTo().innerFrame("parentFrame");
     $("frame").shouldHave(name("childFrame_1"));
   }
 
-  @Test
-  @Video
+  @RepeatedTest(100)
   void waitsUntilFrameAppears_byTitle() {
     switchTo().frame("leftFrame");
     $("h1").shouldHave(text("Page with dynamic select"));
-  }
-
-  @Test
-  @Video
-  void waitsUntilFrameAppears_byIndex() {
-    assumeFalse(browser().isHtmlUnit());
-
-    switchTo().frame(2);
-
-    $("h1").shouldHave(text("Page with JQuery"));
-    assertThat(driver().source()).contains("Test::jquery");
   }
 }
--- statics/src/test/java/integration/customcommands/MySelenideElement.java
+++
@@ -1,9 +0,0 @@
-package integration.customcommands;
-
-import com.codeborne.selenide.SelenideElement;
-
-public interface MySelenideElement extends SelenideElement {
-  MySelenideElement tripleClick();
-
-  MySelenideElement quadrupleClick();
-}
