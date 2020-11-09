869e3b00e5ab063c2716acb7939830c8bcc8c4df
Andrei Solntsev <andrei.solntsev@gmail.com>
2020-11-10 00:21:11 +0200

    increase timeout

--- src/test/java/integration/FramesTest.java
+++ src/test/java/integration/FramesTest.java
@@ -14,6 +14,7 @@ import static org.junit.jupiter.api.Assumptions.assumeFalse;
 final class FramesTest extends ITest {
   @BeforeEach
   void openPage() {
+    setTimeout(5000);
     openFile("page_with_frames.html");
   }
 
